3eb995d7f58699d981ff9c197630c9c4787063f1
Eugene <exsoulck@gmail.com>
2023-08-21 15:50:07 -0400

    Organize some parts of code

--- tests/test_lambdatest.py
+++ tests/test_lambdatest.py
@@ -4,7 +4,7 @@ import pytest
 from utils.file_utils import read_data_file
 
 
-@allure.suite("Lambdatest API tests")
+@allure.suite("Conventers tests")
 @allure.title("JSON to XML conversion")
 @pytest.mark.parametrize("file_name", ["1", "2"])
 def test_json_to_xml(lambdatest_service, file_name):
@@ -19,7 +19,7 @@ def test_json_to_xml(lambdatest_service, file_name):
         assert actual_xml == expected_xml
 
 
-@allure.suite("Lambdatest API tests")
+@allure.suite("Text extraction")
 @allure.title("Extract text from JSON")
 @allure.severity(allure.severity_level.CRITICAL)
 @allure.tag("smoke")
@@ -46,6 +46,7 @@ def test_extract_text_from_json(lambdatest_service, file_name):
         assert actual_text == expected_text
 
 
+@allure.suite("Conventers tests")
 @allure.title("Yaml validator")
 @allure.link("https://www.lambdatest.com/free-online-tools/yaml-validator")
 @pytest.mark.parametrize("file_name", ["1", "2"])
@@ -60,6 +61,7 @@ def test_yaml_validator(lambdatest_service, file_name):
         assert request.json()["message"] == "Valid YAML"
 
 
+@allure.suite("Conventers tests")
 @allure.title("JSON to YAML converter")
 @allure.link("https://www.lambdatest.com/free-online-tools/json-to-yaml")
 @pytest.mark.parametrize("file_name", ["1", "2"])
@@ -70,14 +72,13 @@ def test_json_to_yaml(lambdatest_service, file_name):
         str_output = str(output_yaml)
 
     with allure.step("Send post request to json to yaml"):
-        request = lambdatest_service.json_to_yaml(input_json)
-
+        request = lambdatest_service.json_to_yaml(input_json).json()["data"]
 
     with allure.step("Compare generated yaml file"):
-        x = request.json()["data"]
-        print(str(x))
-        assert str(x) == str_output
+        assert str(request) == str_output
 
+
+@allure.suite("Conventers tests")
 @allure.title("XML to YAML converter")
 @allure.link("https://www.lambdatest.com/free-online-tools/json-to-yaml")
 @pytest.mark.parametrize("file_name", ["1", "2"])
@@ -85,14 +86,14 @@ def test_xml_yaml(lambdatest_service, file_name):
     with allure.step("Prepare test data"):
         input_xml = read_data_file(f"xml/{file_name}.xml")
         output_yaml = read_data_file(f"yaml/{file_name}.yml")
+        expected_text = lambdatest_service.extract_text_from_json(output_yaml)
 
-    with allure.step("Send POST request to xml to yaml converter"):
-        response = lambdatest_service.xml_to_yaml(input_xml)
+    with allure.step("Send POST request to xml to yaml converter , extract text from "):
+        response = lambdatest_service.xml_to_yaml(input_xml).json()["data"]
 
     with allure.step("Compare results"):
-        actual_data = response.json()["data"]
-        actual_text = lambdatest_service.extract_text_from_json(actual_data)
-        expected_text = lambdatest_service.extract_text_from_json(output_yaml)
+        actual_text = lambdatest_service.extract_text_from_json(response)
+
         assert actual_text == expected_text
 
 # HOME TASK
@@ -101,4 +102,3 @@ def test_xml_yaml(lambdatest_service, file_name):
 # https://www.lambdatest.com/free-online-tools/json-to-yaml
 # https://www.lambdatest.com/free-online-tools/xml-to-yaml
 # https://www.lambdatest.com/free-online-tools/yaml-to-json
-
